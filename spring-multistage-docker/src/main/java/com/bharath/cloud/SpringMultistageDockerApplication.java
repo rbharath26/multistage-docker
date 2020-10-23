bac747d9f6399e2d407408cf951d509aa758b7f8
Bharath Raghavan <rbharath26@gmail.com>
2020-10-23 05:33:55 -0500

    Add files via upload
    multistage docker sample

---
+++ spring-multistage-docker/src/main/java/com/bharath/cloud/SpringMultistageDockerApplication.java
@@ -0,0 +1,13 @@
+package com.bharath.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMultistageDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMultistageDockerApplication.class, args);
+	}
+
+}
